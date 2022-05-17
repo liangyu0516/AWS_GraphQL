07554792d1deb3a323d054a8077a7978783a5573
liangyuchen <cy9815526@gmail.com>
2022-05-18 00:42:14 +0800

    feat: create product type and query

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,24 @@ const { ApolloServer, gql } = require('apollo-server');
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
+  }
+
+  type Product {
+    id: Int
+    category: String
+    title: String
+    description: String
+    price: Int
+    texture: String
+    wash: String
+    place: String
+    note: String
+    story: String
+    main_image: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -17,5 +29,39 @@ const typeDefs = gql`
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    product: Product
+    products: [Product]
   }
 `;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+  });
+  
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
